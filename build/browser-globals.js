05c4f2315ad18ad0c970c5e4a003a6100fd49446
Spadar Shut <sp.shut@gmail.com>
2021-01-15 17:00:51 +0300

    add babel eslint parser to support top-level await

--- build/browser-globals.js
+++ build/browser-globals.js
@@ -185,16 +185,23 @@ const SELECTORS = {
     .storyStream ~ * [role="progressbar"][aria-busy="true"],
     .storyStream ~ * [data-sigil ~= "m-loading-indicator-root"]
   `,
-  // single post
+  img: 'i[role=img]',
+  // single post, in feed or standalone
   postRoot: '[data-sigil="m-story-view"]',
   postLink: '[data-sigil="feed-ufi-trigger"]',
   postTime: '[data-sigil="m-feed-voice-subtitle"] abbr',
+  postTextRoot: '.story_body_container > header + div',
+  postMediaRoot: '.story_body_container > header + div + div',
   postShares: '[data-sigil="feed-ufi-sharers"]',
   postReactions: 'a[href*="ufi/reaction"]',
+  postReactionsSentence: '[data-sigil="reactions-sentence-container"]',
   postCommentsRoot: '[data-sigil="m-mentions-expand"]',
   postComment: '[data-sigil="comment"]',
   postCommentBody: '[data-sigil="comment-body"]',
-  postMoreLink: '[data-sigil="more"]'
+  postMoreLink: '[data-sigil="more"]',
+  postSharedPortRoot: '.story_body_container .story_body_container',
+  // Full post
+  postCardContent: '.story_body_container > header + div [aria-hidden=true][role=presentation][style]'
 };
 exports.SELECTORS = SELECTORS;
 },{}]},{},["ae279a62d6699cbcfbcb300b7dac0546"], null)
